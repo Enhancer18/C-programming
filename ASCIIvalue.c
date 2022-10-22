5c00ca210a4d4fb63ad94ee75fbd614b1de9577b
Enhancer18 <102911149+Enhancer18@users.noreply.github.com>
2022-10-22 17:35:53 +0530

    displays the ASCII value of the required alphabet.

---
+++ ASCIIvalue.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+    char alphabet;
+    printf("enter a aphabet: ",alphabet);
+    scanf(" %c",&alphabet);
+    printf("enter the ASCII value:%d",alphabet);
+    
+    return 0;
+}
